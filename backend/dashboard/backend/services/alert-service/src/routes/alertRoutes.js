d22cdc2e1d2ceef2de7e4832fc715148148b71fe
Thisen2002 <e21231@eng.pdn.ac.lk>
2025-09-15 11:12:04 +0530

    adding backend of organizer dashboard

--- backend/dashboard/backend/services/alert-service/src/routes/alertRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { sendAlert } = require('../controllers/alertController');
-const router = express.Router();
-
-router.post('/', sendAlert);
-
-module.exports = router;
