a3411fff9fef05287ac8ce5bf4cf857663fa69dc
manoputh <e21292@eng.pdn.ac.lk>
2025-09-20 20:53:53 +0530

    remove some charts

--- src/pages/Heatmap/HeatMapAnalysis/OverallTrendChart.js
+++ src/pages/Heatmap/HeatMapAnalysis/OverallTrendChart.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-// No-op OverallTrendChart: intentionally renders nothing
-const OverallTrendChart = () => null;
+// DISABLED: Overall Trend Chart has been completely removed per user request
+// This chart was showing "Overall Crowd Trend" with "Real-time occupancy data across all buildings"
+const OverallTrendChart = () => {
+  console.warn('OverallTrendChart component has been disabled - chart should not render');
+  return null;
+};
 
 export default OverallTrendChart;
